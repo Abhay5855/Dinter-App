1efe799d20471928c7a77c03733b7a6fdbefbba1
Abhay5855 <ap3919529@gmail.com>
2022-02-08 21:37:55 +0530

    docs : creating a google logout function to log out of the application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
 import { UserAuthContextProvider } from "./hooks/useAuth";
 import Privateroutes from "./components/routes/Privateroutes";
-import Publicroutes from "./components/routes/Publicroutes";
-
+import Publicroutes from "./components/routes/Publicroutes";import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import ChatScreen from "./components/ChatScreen";
+import HomeScreen from "./components/HomeScreen";
+import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
       <UserAuthContextProvider>
-        <Privateroutes />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/chat" element={<ChatScreen />}></Route>
 
-        <Publicroutes />
+            <Route path="/home" element={<HomeScreen />}></Route>
+            <Route path="/" element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
+
+        {/* <Privateroutes />
+
+        <Publicroutes /> */}
       </UserAuthContextProvider>
     </div>
   );
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,22 +1,42 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { GoogleAuthProvider, signInWithPopup , onAuthStateChanged } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut ,
+} from "firebase/auth";
 import { auth } from "../firebase";
 
 const userAuthContext = createContext();
 
 export function UserAuthContextProvider({ children }) {
+  //  state to check the user
+  const [user, setUser] = useState(null);
+
+  // google signin function
   function googleSignIn() {
     const googleAuthProvider = new GoogleAuthProvider();
     return signInWithPopup(auth, googleAuthProvider);
   }
 
+  // Check wether the user is there or not
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
   return (
-    <userAuthContext.Provider value={{ user: null, googleSignIn }}>
+    <userAuthContext.Provider value={{ user, googleSignIn }}>
       {children}
     </userAuthContext.Provider>
   );
 }
-
+//custom function with the context passed
 export function useUserAuth() {
   return useContext(userAuthContext);
 }
