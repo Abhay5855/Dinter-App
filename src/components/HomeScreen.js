0521033008a7199e4f43bf8ef899198005761520
Abhay5855 <ap3919529@gmail.com>
2022-02-08 22:07:05 +0530

    docs : navigate between home AND login using Navigate and logout function

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,12 +2,38 @@
 
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserAuth } from '../hooks/useAuth';
 
 const HomeScreen = () => {
+
+  const { googleSignout } = useUserAuth();
+
+
+  const navigate = useNavigate();
+
+    const logout = async(e) => {
+        e.preventDefault();
+
+        try {
+            
+            await googleSignout();
+            navigate('/');
+            console.log('logout successful');
+        }
+        catch(err) {
+              console.log(err);
+        }
+
+    }
+
+
   return (
 
     <div>
-        home
+        <button onClick={logout}>
+          Logout
+        </button>
     </div>
   )
   
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -12,12 +12,26 @@ const userAuthContext = createContext();
 export function UserAuthContextProvider({ children }) {
   //  state to check the user
   const [user, setUser] = useState(null);
+  const [error , setError] = useState(null);
 
   // google signin function
   function googleSignIn() {
     const googleAuthProvider = new GoogleAuthProvider();
     return signInWithPopup(auth, googleAuthProvider);
   }
+
+  //google signout 
+   function googleSignout(){
+
+    signOut(auth).then(() => {
+      // Sign-out successful.
+
+    }).catch((error) => {
+      // An error happened.
+      setError(error);
+    });
+
+   }
 
   // Check wether the user is there or not
   useEffect(() => {
@@ -31,7 +45,7 @@ export function UserAuthContextProvider({ children }) {
   }, []);
 
   return (
-    <userAuthContext.Provider value={{ user, googleSignIn }}>
+    <userAuthContext.Provider value={{ user, googleSignIn , googleSignout }}>
       {children}
     </userAuthContext.Provider>
   );
