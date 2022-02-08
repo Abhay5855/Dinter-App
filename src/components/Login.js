d65cfb01e945f9d61a18b18da0a4bc03f6b8b7fb
Abhay5855 <ap3919529@gmail.com>
2022-02-08 14:59:04 +0530

    docs : creating a new function googleSignin and creating a new instance of provider and passing the auth and provider to signupwithpop up method, alos passing the value to the context provider

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,29 @@
 
 
 import React from 'react';
+import GoogleButton from "react-google-button";
 
 const Login = () => {
 
     console.log('Heena');
+
+    function handleGoogleSignIn() {
+            
+    }
   return (
+
+    <>
 
     <div>
         Login
     </div>
+
+     <GoogleButton
+            className="g-btn"
+            type="dark"
+            onClick={handleGoogleSignIn}
+          />
+          </>
   )
   
 };
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,32 +1,30 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext } from "react";
+import {signInWithPopup, GoogleAuthProvider, GoogleAuthProvider } from "firebase/auth";
+import {auth} from '../firebase'
+
+export const AuthProvider = ({ children }) => {
+  const AuthContext = createContext({});
 
 
+  //creating a function for google signin
+
+  function googleSignin() {
+
+           const GoogleAuthProvider = new GoogleAuthProvider();
+
+           return signInWithPopup(auth , GoogleAuthProvider);
+  }
 
 
-
-
-export const AuthProvider = ({children}) => {
-
-    
-const AuthContext = createContext({});
-
-
-       return (
-           <>
-           <AuthContext.Provider value={{user : 'Heena'}}>
-           {children}
-
-           </AuthContext.Provider>
-
-           
-           </>
-       )
-}
-
+  return (
+    <>
+      <AuthContext.Provider value={googleSignin}>
+        {children}
+      </AuthContext.Provider>
+    </>
+  );
+};
 
 export default function useAuth() {
-
-    return useContext(AuthProvider);
-
+  return useContext(AuthProvider);
 }
-
