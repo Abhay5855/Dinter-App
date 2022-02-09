17a12ff662c673020136524b74c78464371b54eb
Abhay5855 <ap3919529@gmail.com>
2022-02-09 23:07:46 +0530

    docs : Adding a label heading and Input filds to complete your Registeration process

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,12 +1,35 @@
-
-
-
-import React from 'react'
+import React from "react";
 
 const Modal = () => {
   return (
-    <div>Modal</div>
-  )
-}
+    <div className="modal__container">
+      <div>
+        <p>Step 1: The Profile Pic</p>
 
-export default Modal
+        <input placeholder="Enter A Profile Pic URL" type="text" />
+      </div>
+      <div>
+        <p>Step 2: The Job</p>
+
+        <input placeholder="Enter your job" type="text" />
+      </div>
+      <div>
+        <p>Step 3: Github/Portfolio URL</p>
+
+        <input placeholder="Enter Your Github/Portfolio URL" type="text" />
+      </div>
+      <div>
+        <p>Step 4: Enter Age</p>
+
+        <input placeholder="Enter Your Age" type="text" />
+      </div>
+      <div>
+        <p>Step 5: Enter Total Experience</p>
+
+        <input placeholder="Enter A Profile Pic URl" type="text" />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
