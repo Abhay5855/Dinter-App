f0bb29ad85a376ebb35439622eafeb1e7ee8ddeb
Abhay5855 <ap3919529@gmail.com>
2022-02-10 00:55:37 +0530

    adding db to firebase

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,71 +1,89 @@
 import React, { useState } from "react";
 import "./modal.css";
 import { useUserAuth } from "../../hooks/useAuth";
+import { doc, setDoc } from "firebase/firestore"; 
 
 const Modal = () => {
+  // Calling the user
+  const { user } = useUserAuth();
 
-        // Calling the user 
-        const {user} = useUserAuth();
+  //calling the states
+  const [profile, setProfile] = useState("");
+  const [devlink, setDevlink] = useState("");
+  const [age, setAge] = useState("");
+  const [experience, setExperience] = useState("");
+  const [job, setJob] = useState("");
 
-        //calling the states
-        const [profile , setProfile] = useState('')
-        const [devlink , setDevlink] = useState('')
-        const [age , setAge] = useState('')
-        const [experience , setExperience] = useState('');
-        const [job , setJob] = useState('');
-
-      const handleSubmit = (e) => {
-
-         e.preventDefault();
-
-      }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="modal__container">
+      <div className="modal__logo">
+        <img
+          src="https://tinder.com/static/build/6dcd593a7d278a1a1e52eff1f03365bb.svg"
+          alt="logo"
+        />
+      </div>
 
-        <div className="modal__logo">
-            <img src='https://tinder.com/static/build/6dcd593a7d278a1a1e52eff1f03365bb.svg' alt='logo'/>
+      {/* <p className="welcome">Welcome {user.displayName}</p> */}
+
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <div className="modal__links">
+          <p>Step 1: The Profile Pic</p>
+
+          <input
+            placeholder="Enter A Profile Pic URL"
+            type="text"
+            value={profile}
+            onChange={(e) => setProfile(e.target.value)}
+          />
+        </div>
+        <div className="modal__links">
+          <p>Step 2: The Job</p>
+
+          <input
+            placeholder="Enter your job"
+            type="text"
+            value={job}
+            onChange={(e) => setJob(e.target.value)}
+          />
+        </div>
+        <div className="modal__links">
+          <p>Step 3: Github/Portfolio URL</p>
+
+          <input
+            placeholder="Enter Github/Portfolio URL"
+            type="text"
+            value={devlink}
+            onChange={(e) => setDevlink(e.target.value)}
+          />
+        </div>
+        <div className="modal__links">
+          <p>Step 4: Enter Age</p>
+
+          <input
+            placeholder="Enter Your Age"
+            type="text"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <div className="modal__links">
+          <p>Step 5: Enter Total Experience</p>
+
+          <input
+            placeholder="Enter Experience In Yrs"
+            type="text"
+            value={experience}
+            onChange={(e) => setExperience(e.target.value)}
+          />
         </div>
 
-        {/* <p className="welcome">Welcome {user.displayName}</p> */}
-
-        <form autoComplete="off" onSubmit={handleSubmit}>
-
-
-
-        
-
-      <div className="modal__links">
-        <p>Step 1: The Profile Pic</p>
-
-        <input placeholder="Enter A Profile Pic URL" type="text" />
-      </div>
-      <div className="modal__links">
-        <p>Step 2: The Job</p>
-
-        <input placeholder="Enter your job" type="text" />
-      </div>
-      <div className="modal__links">
-        <p>Step 3: Github/Portfolio URL</p>
-
-        <input placeholder="Enter Github/Portfolio URL" type="text" />
-      </div>
-      <div className="modal__links">
-        <p>Step 4: Enter Age</p>
-
-        <input placeholder="Enter Your Age" type="text" />
-      </div>
-      <div className="modal__links">
-        <p>Step 5: Enter Total Experience</p>
-
-        <input placeholder="Enter A Profile Pic URL" type="text" />
-      </div>
-     
-          
-      <button className="update__btn" type="submit">Add Changes</button>
-
-     
-
+        <button className="update__btn" type="submit">
+          Add Changes
+        </button>
       </form>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 
 // Your web app's Firebase configuration
@@ -14,5 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
 
 export const auth = getAuth(app);
