e3deda8bb4d2d72bc8706c8c056cba91fe11c743
Abhay5855 <ap3919529@gmail.com>
2022-02-09 23:32:19 +0530

    style : adding the style to text and the input feilds

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,32 +1,38 @@
 import React from "react";
+import "./modal.css";
 
 const Modal = () => {
   return (
     <div className="modal__container">
-      <div>
+      <div className="modal__links">
         <p>Step 1: The Profile Pic</p>
 
         <input placeholder="Enter A Profile Pic URL" type="text" />
       </div>
-      <div>
+      <div className="modal__links">
         <p>Step 2: The Job</p>
 
         <input placeholder="Enter your job" type="text" />
       </div>
-      <div>
+      <div className="modal__links">
         <p>Step 3: Github/Portfolio URL</p>
 
-        <input placeholder="Enter Your Github/Portfolio URL" type="text" />
+        <input placeholder="Enter Github/Portfolio URL" type="text" />
       </div>
-      <div>
+      <div className="modal__links">
         <p>Step 4: Enter Age</p>
 
         <input placeholder="Enter Your Age" type="text" />
       </div>
-      <div>
+      <div className="modal__links">
         <p>Step 5: Enter Total Experience</p>
 
         <input placeholder="Enter A Profile Pic URl" type="text" />
+      </div>
+      <div className="modal__links">
+        <p>Step 6: Enter Social Networks</p>
+
+        <input placeholder="Enter A Linkedin/Twitter URL" type="text" />
       </div>
     </div>
   );
