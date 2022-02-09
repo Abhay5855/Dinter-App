a4ce188be71b2792443120026ea0a30dd7a320c6
Abhay5855 <ap3919529@gmail.com>
2022-02-09 16:00:31 +0530

    feat : adding the react-tinder-cards component in home screen

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,83 +1,68 @@
-
-
-
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useUserAuth } from '../hooks/useAuth';
-import { Swipeable, direction } from 'react-deck-swiper';
-import './home.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserAuth } from "../hooks/useAuth";
+import TinderCard from "react-tinder-card";
+import "./home.css";
 
 const HomeScreen = () => {
-
- 
-
   const { googleSignout, user } = useUserAuth();
-
-  
-     
-  
-
 
   const navigate = useNavigate();
 
-    const logout = async(e) => {
-        e.preventDefault();
+  const logout = async (e) => {
+    e.preventDefault();
 
-        try {
-            
-            await googleSignout();
-            navigate('/');
-            console.log('logout successful');
-        }
-        catch(err) {
-              console.log(err);
-        }
-
+    try {
+      await googleSignout();
+      navigate("/");
+      console.log("logout successful");
+    } catch (err) {
+      console.log(err);
     }
+  };
+
+
+  //tinder cards
+  
+const onSwipe = (direction) => {
+  console.log('You swiped: ' + direction)
+}
+
+const onCardLeftScreen = (myIdentifier) => {
+  console.log(myIdentifier + ' left the screen')
+}
 
 
   return (
-
-    <div className='home__container'>
-
+    <div className="home__container">
       <nav>
-
-        <div className='home__avatar'>
-
-          <img src={user.photoURL} alt='avatar'/>
-
+        <div className="home__avatar">
+          <img src={user.photoURL} alt="avatar" />
         </div>
-        <div className='home__logo'>
-
-          <img src='https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png' alt='logo'/>
-
+        <div className="home__logo">
+          <img
+            src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png"
+            alt="logo"
+          />
         </div>
-        <div className='home__chat'>
-               
-          
-
-        </div>
-
-
+        <div className="home__chat"></div>
       </nav>
 
-      <div>
-      <Swipeable>
-      <div className="card">
-        Your card content here
+      {/* react card */}
+
+      <div className="home__cards">
+        <TinderCard
+          onSwipe={onSwipe}
+          onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+          preventSwipe={["top", "bottom"]}
+        >
+          <div className="card__image">
+          
+          </div>
+        </TinderCard>
       </div>
-    </Swipeable>
-      </div>
-
-
-
-
-       
-
-       
     </div>
-  )
-  
+  );
 };
 
 export default HomeScreen;
