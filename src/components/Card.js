47cb7f2c6572bcf13e201d09a4354b07be175a3e
Abhay5855 <ap3919529@gmail.com>
2022-02-11 15:28:24 +0530

    docs : fetching the user from the database inside the useEffect hook and unsubscribe the change set the result of the snapshot in persons state and lastly rendering the profiles object on the screen

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import "./tindercard.css";
 import TinderCard from "react-tinder-card";
-import { onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { doc} from "firebase/firestore"; 
 import { db } from "../firebase";
 import { useUserAuth } from "../hooks/useAuth";
@@ -11,32 +11,14 @@ const Card = () => {
 
    const {user} = useUserAuth();
 
-  const [people, setPeople] = useState([
-    {
-      name: "Rutkar",
-      url: "https://scontent.fnag4-1.fna.fbcdn.net/v/t1.6435-9/p843x403/117592367_657281824883893_1397295896497917495_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=84a396&_nc_ohc=9rc2Vwyq9HAAX-uwB38&_nc_oc=AQlPQHkuDL7gNX0BUiOgmLbQpl4Pnk6cr8VH4852HSu6dV1FQBwUwFGityAeeCFniDZ9jQSjGTBbxMvi9uJU2D13&tn=L-1wmDopkrB64zdZ&_nc_ht=scontent.fnag4-1.fna&oh=00_AT-71_mge8wbGDXD9gK7XlvINIrSlbgeIndeN7-ucdOIpw&oe=622B53BD",
-      age: 24,
-      Github: "rutkarverma07",
-    },
-    {
-      name: "Sanika",
-      url: "https://scontent.fnag4-1.fna.fbcdn.net/v/t1.6435-9/s851x315/145050096_888591505014385_1337213454963259110_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=da31f3&_nc_ohc=0rSFz2LQOyUAX9zsDha&_nc_ht=scontent.fnag4-1.fna&oh=00_AT_cxIsQjx8JtOH8gYUGtj2ulDHRLZf_HrtJdJSJRkLRtQ&oe=622A7383",
-      age: 23,
-      Github: "Sanika02",
-    },
+   const [profiles , setProfiles] = useState([]);
 
-    {
-      name: "Abhay",
-      url: "https://scontent-pnq1-1.xx.fbcdn.net/v/t31.18172-8/s851x315/29744283_2051089331839146_4452588607893092629_o.jpg?_nc_cat=104&ccb=1-5&_nc_sid=da31f3&_nc_ohc=RRFk06kCdBIAX-DQF-A&_nc_ht=scontent-pnq1-1.xx&oh=00_AT8uGnVErhiDaJG4zt58ZMIBFRMz2bLQBrufuPU9BgNQMg&oe=622A150E",
-      age: 23,
-      Github: "Abhay5855",
-    },
-  ]);
+ 
 
 
   const navigate = useNavigate();
 
-  // get the stored users from the firebase database;
+  // check if the users are present  from the firebase database;
   useLayoutEffect(() => {
 
     onSnapshot(doc(db, 'users' , user.uid) , (snapshot) => {
@@ -53,17 +35,44 @@ const Card = () => {
 
 
   },[])
+
+  // get the users from fireabsee databvase
+  useEffect(() => {
+       let unsubscribe;
+
+       const fetchData = async() => {
+
+           unsubscribe = onSnapshot(collection(db , 'users'), snapshot => {
+
+                 setProfiles(
+                   snapshot.docs.map(doc => ({
+
+                        id : doc.id,
+                        ...doc.data(),
+                   }))
+                 )
+           })
+
+       }
+
+       fetchData();
+
+       return unsubscribe;
+  },[])
   
+
+  console.log(profiles , 'profiles')
 
   //function to check the direaction of the swipe
   const onSwipe = (direction) => {
     console.log("You swiped: " + direction);
     console.log("swipe");
   };
+
   return (
     <>
       <div className="home__cards">
-        {people.map((item) => (
+        {profiles.map((item) => (
           <TinderCard
             key={item.name}
             onSwipe={onSwipe}
@@ -73,7 +82,7 @@ const Card = () => {
             <div className="tindercard__container">
               <div
                 className="card__image"
-                style={{ backgroundImage: `url(${item.url})` }}
+                style={{ backgroundImage: `url(${item.photoURL})` }}
               ></div>
 
               <div>
@@ -87,7 +96,7 @@ const Card = () => {
 
                   <div className="details2">
                     <span>Age: {item.age}</span>
-                    <span>Job: MihoLearn</span>
+                    <span>Job: {item.job}</span>
                   </div>
                 </div>
               </div>
