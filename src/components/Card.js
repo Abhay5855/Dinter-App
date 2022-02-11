9fec106289a1c57dfa78e2c7290635ebf46833e4
Abhay5855 <ap3919529@gmail.com>
2022-02-11 21:33:21 +0530

    docs : creating a function onSwipe and pass the id and index as arguments inside this function if the user swipes to left the check for the id in profiles array if that match then store the result in firestore database with passes that consist user information which is swipe to the left

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React, { useEffect, useLayoutEffect, useState } from "react";
 import "./tindercard.css";
 import TinderCard from "react-tinder-card";
 import { collection, onSnapshot } from "firebase/firestore";
-import { doc } from "firebase/firestore";
+import { doc , setDoc} from "firebase/firestore";
 import { db } from "../firebase";
 import { useUserAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
@@ -10,14 +10,36 @@ import { useNavigate } from "react-router-dom";
 const Card = () => {
   const { user } = useUserAuth();
 
-  const [profiles, setProfiles] = useState([]);
+   const [profiles , setProfiles] = useState([]);
+
+  // const [profiles, setProfiles] = useState([
+  //   {
+  //     name : 'Abhay',
+  //     age : 23,
+  //     job : 'react dev',
+  //     photoURL : 'https://scontent.fpnq7-4.fna.fbcdn.net/v/t1.6435-9/s851x315/68615542_2382368328711243_690095807315050496_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=da31f3&_nc_ohc=JeISkiuOsWIAX8_gI9Z&_nc_ht=scontent.fpnq7-4.fna&oh=00_AT_k2cODgCbo2_wkDetFdpXZ3skj1Y3VSFl3PaA0aSIXDw&oe=622D01A5'
+  //   },
+
+  //   {
+  //     name : 'Sanika',
+  //     age : 24,
+  //     job : 'analyst',
+  //     photoURL : 'https://www.facebook.com/photo/?fbid=1001599010380300&set=pob.100015906306605'
+  //   },
+  //   {
+  //     name : 'rutkar',
+  //     age : 25,
+  //     job : 'team lead',
+  //     photoURL : 'https://scontent.fpnq7-4.fna.fbcdn.net/v/t1.6435-9/s851x315/117592367_657281824883893_1397295896497917495_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=da31f3&_nc_ohc=bKmjqz30qeUAX9OK5KN&_nc_oc=AQkE8LEBJpzZgmZaeo6jyKnxTZM2mNxugmP_67c11iJp1QyvcZzKA3GkHnmaCEEU1AnMyzdgY6tAPM-5waz7IUSY&_nc_ht=scontent.fpnq7-4.fna&oh=00_AT8GHXYGC0qrhrS4fqU0AF0vPFBMMVJP_P7i1tQs2R98hA&oe=622C2BB7'
+  //   }
+  // ]);
 
   const navigate = useNavigate();
 
   // check if the users are present  from the firebase database;
   useLayoutEffect(() => {
     onSnapshot(doc(db, "users", user.uid), (snapshot) => {
-      console.log(snapshot);
+    
 
       if (!snapshot.exists) {
         navigate("/modal");
@@ -25,7 +47,7 @@ const Card = () => {
     });
   }, []);
 
-  // get the users from fireabsee databvase
+  // get the users from fireabsee database
   useEffect(() => {
     let unsubscribe;
 
@@ -33,7 +55,7 @@ const Card = () => {
       unsubscribe = onSnapshot(collection(db, "users"), (snapshot) => {
         setProfiles(
           snapshot.docs
-          .filter((doc) => doc.id !== user.uid)
+          // .filter((doc) => doc.id !== user.uid)
           .map((doc) => ({
             id: doc.id,
             ...doc.data(),
@@ -47,21 +69,45 @@ const Card = () => {
     return unsubscribe;
   }, []);
 
-  console.log(profiles, "profiles");
+  
 
   //function to check the direaction of the swipe
-  const onSwipe = (direction) => {
-    console.log("You swiped: " + direction);
-    console.log("swipe");
+  const onSwipe = (direction, idx) => {
+
+       if(direction === 'left'){
+
+          //  if(!profiles[id]) return;
+            
+
+           const userSwipped = profiles[idx];
+
+           console.log(`You Passed ${userSwipped.job}`);
+         
+            setDoc(doc(db, 'users' , user.uid , 'passes' , userSwipped.id), userSwipped);
+       
+     
+
+
+
+       } 
+
+       if(direction === 'right'){
+
+
+       }
+        
   };
 
   return (
     <>
       <div className="home__cards">
-        {profiles.map((item) => (
+        {profiles.map((item , idx) => (
           <TinderCard
             key={item.name}
-            onSwipe={onSwipe}
+            onSwipe={(direction) => {
+                  onSwipe(direction, idx);
+            }}
+           
             preventSwipe={["up", "down"]}
             className="swipe"
           >
